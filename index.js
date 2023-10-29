e27e3d60b5a9aac11804404d963e8ffcb0690b6c
Sifatul Islam <mdshepatul.islam@gmail.com>
2023-10-29 14:25:57 +0600

    delete product added

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ async function run() {
 
             const options = {
                 // include only the `title` and `image` etc
-                projection: {title: 1, price: 1, service_id: 1},
+                projection: { title: 1, price: 1, service_id: 1, img: 1 },
             };
 
             const result = await serviceCollection.findOne(query, options);
@@ -60,8 +60,32 @@ async function run() {
 
         // bookings
 
-        app.post('/bookings', async (req, res) =>{
+
+        app.get('/bookings', async (req, res) => {
+            console.log(req.query.email);
+            let query = {};
+            if (req.query?.email) {
+                query = { email: req.query.email }
+            }
+
+            const result = await bookingCollection.find(query).toArray();
+            res.send(result)
+        })
+
+        app.post('/bookings', async (req, res) => {
             const booking = req.body;
+            console.log(booking);
+            const result = await bookingCollection.insertOne(booking);
+            res.send(result);
+        });
+
+
+
+        app.delete('/bookings/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = {_id: new ObjectId(id)}
+            const result = await bookingCollection.deleteOne(query);
+            res.send(result);
         })
 
 
