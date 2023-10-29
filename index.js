ee2a05010afa9a4795bf86bbc802dabefaa28d58
Sifatul Islam <mdshepatul.islam@gmail.com>
2023-10-29 16:11:24 +0600

    update product added

--- index.js
+++ index.js
@@ -80,6 +80,20 @@ async function run() {
         });
 
 
+        app.patch('/bookings/:id', async(req, res) =>{
+            const id = req.params.id;
+            const filter = {_id: new ObjectId(id)}
+            const updateBooking = req.body;
+            console.log(updateBooking);
+            const updateDoc= {
+                $set: {
+                    status: updateBooking.status
+                },
+            };
+            const result =await bookingCollection.updateOne(filter, updateDoc);
+            res.send(result)
+        })
+
 
         app.delete('/bookings/:id', async (req, res) => {
             const id = req.params.id;
